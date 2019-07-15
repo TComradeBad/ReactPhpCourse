656075e3a1ccef1f8c47faa55d4898524294339a
TComradeBad <TComradeBad@yandex.ru>
2019-07-15 15:33:15 +0000

    go

--- index.php
+++ index.php
@@ -1,10 +1,22 @@
 <?php
 
-require __DIR__.'\vendor\autoload.php';
+require __DIR__.'/vendor/autoload.php';
 
 
 $loop = React\EventLoop\Factory::create();
-$fs = React\Filesystem\Filesystem::create($loop);
 
+$server = new React\Http\Server(function (Psr\Http\Message\ServerRequestInterface $request) {
+    return new React\Http\Response(
+        200,
+        array('Content-Type' => 'text/plain'),
+        "Hello World!\n"
+    );
+});
+
+$socket = new React\Socket\Server("192.168.33.10:8080", $loop);
+$server->listen($socket);
+
+echo "SERVER RUN";
+$loop->run();
 
 
